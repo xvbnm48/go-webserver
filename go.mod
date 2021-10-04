a5dbac3484b8246d69eccbcb9ee4f4a2c252e72d
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-10-04 22:38:25 +0700

    konfigurasi with docker

--- go.mod
+++ go.mod
@@ -1,15 +1,19 @@
 module go-webserver
 
-go 1.17
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.19.0
+	github.com/golang-jwt/jwt/v4 v4.1.0
+	github.com/lib/pq v1.10.2
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	xorm.io/xorm v1.2.5
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
-	github.com/gofiber/fiber/v2 v2.19.0 // indirect
-	github.com/gofiber/jwt/v3 v3.1.1 // indirect
-	github.com/golang-jwt/jwt/v4 v4.1.0 // indirect
+	github.com/gofiber/jwt/v3 v3.1.1
 	github.com/klauspost/compress v1.13.6 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.30.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,103 @@
 package main
 
 import (
+	"go-webserver/data"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/golang-jwt/jwt/v4"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type SignupRequest struct {
+	Name     string
+	Email    string
+	Password string `json:"*"`
+}
+
+type loginRequest struct {
+	Email    string
+	Password string `json:"*"`
+}
 
 func main() {
 	app := fiber.New()
+	engine, err := data.CreateDBEngine()
+	if err != nil {
+		panic(err)
+	}
 
 	app.Post("/signup", func(c *fiber.Ctx) error {
-		return nil
+		req := new(SignupRequest)
+		if err := c.BodyParser(req); err != nil {
+			return err
+		}
+
+		if req.Email == "" || req.Name == "" || req.Password == "" {
+			return fiber.NewError(fiber.StatusBadRequest, "invalid signup credential")
+		}
+
+		// save this info in the database
+		hash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return err
+		}
+
+		user := &data.User{
+			Name:     req.Name,
+			Email:    req.Email,
+			Password: string(hash),
+		}
+
+		_, err = engine.Insert(user)
+		if err != nil {
+			return err
+		}
+		token, exp, err := createJWTToken(*user)
+		if err != nil {
+			return err
+		}
+
+		// create jwt token
+
+		return c.JSON(fiber.Map{"token": token, "exp": exp, "user": user})
 	})
 
 	app.Post("/login", func(c *fiber.Ctx) error {
-		return nil
-	})
+		req := new(loginRequest)
+		if err := c.BodyParser(req); err != nil {
+			return err
+		}
 
-	app.Get("/private", func(c *fiber.Ctx) error {
+		if req.Email == "" || req.Password == "" {
+			return fiber.NewError(fiber.StatusBadRequest, "invalid signup credential")
+		}
+		user := new(data.User)
+		has, err := engine.Where("email = ?", req.Email).Desc("id").Get(user)
+		if err != nil {
+			return err
+		}
+		if !has {
+			return fiber.NewError(fiber.StatusBadRequest, "invalid login")
+		}
+		token, exp, err := createJWTToken(*user)
+		if err != nil {
+			return err
+		}
+
+		// create jwt token
+
+		return c.JSON(fiber.Map{"token": token, "exp": exp, "user": user})
+	})
+	
+	private := app.Group("/private")
+	private.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
+	private.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"success": true, "path": "private"})
 	})
 
@@ -26,4 +106,20 @@ func main() {
 	})
 
 	log.Fatal(app.Listen(":8080"))
+}
+
+func createJWTToken(user data.User) (string, int64, error) {
+	exp := time.Now().Add(time.Minute * 30).Unix()
+
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = user.Id
+	claims["exp"] = exp
+	t, err := token.SignedString([]byte("secret"))
+
+	if err != nil {
+		return "", 0, err
+	}
+
+	return t, exp, nil
 }
